8fcbc9f8383eb4b350dab1f81f34361c2152fc43
Shrusti454 <160777120+Shrusti454@users.noreply.github.com>
2024-08-07 20:38:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const {MongoClient} = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/save",(req,res) => {
+    const url = mongodb+srv://shrusti047:<password>@doubtapp.5szbucw.mongodb.net/?retryWrites=true&w=majority&appName=doubtapp;
+    const client = new MongoClient(url);
+    const db = client.db("doubtapp");
+    const coll = db.collection("student");
+    const record = {"name": req.body.name, "number":req.body.number, "doubt":req.body.doubt}
+    coll.insertOne(record)
+    .then(result => res.send(result))
+    .catch(error => res.send(error));
+});
+
+app.listen(9000, () => {console.log("ready @ 9000");});
